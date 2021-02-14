8fe8a58d5e78fafd626a54101655f0b2bd856c53
Travis Wahl <twahl1991@gmail.com>
2021-02-13 21:35:06 -0500

    Update Register.java
    added comments to make methods clearer

--- src/storesimulation/Register.java
+++ src/storesimulation/Register.java
@@ -29,48 +29,59 @@ class Register {
         this.regType = t;
     }
    
+    //get the type of register (standard checkout or self checkout)
     String getRegisterType(){
         return this.regType;
     }
  
+    //get the size of the line for a register
     int getLineLength() {
         return this.que.size();
     }
  
+    //put a customer into line for a register
     void enqueue(Customer customer) {
         this.que.add(customer);
         this.totalPassedThru++;//keep track of how many passed thru each register
         checkNewMaxL();
     }
  
+    //remove a customer from the line of a register
     Customer dequeue() {//pop off the next up in line
         return this.que.remove(0);
     }
  
+    //check if the queue is empty
     boolean isEmpty() {
         return this.que.isEmpty();
     }
  
-    Customer peek() {//*view* the next up in line
+    //view the next customer in line (used to see if there is another customer in line)
+    Customer peek() {
         return this.que.get(0);
     }
    
+    //Check the length of a line
     void checkNewMaxL(){
         if (this.que.size() > this.maxL) this.maxL = this.que.size();
     }
    
+    //get the time it takles to scan an item for a given customer
     double getScanTime() {
         return this.itemScanTime; 
     }
  
+    //get the time it takes to pay for a given customer
     double getPayTime() {
         return this.payTime;
     }
    
+    //used to store the length into maxL
     double getMaxLength(){
         return this.maxL;
     }
    
+    //get the number of customers that used a given register
     int getTotalPassThru(){
         return this.totalPassedThru;
     }
