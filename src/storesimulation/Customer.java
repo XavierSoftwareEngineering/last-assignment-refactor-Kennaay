9dcb81c7b16ca49478e1a36224bfaade8dfbc867
Travis Wahl <twahl1991@gmail.com>
2021-02-13 21:41:37 -0500

    Update Customer.java
    added comments to make methods clearer

--- src/storesimulation/Customer.java
+++ src/storesimulation/Customer.java
@@ -66,34 +66,42 @@ class Customer {
         this.avgSelectionTime = avgSelectionTime;
     }
  
+    //sets the checkout line that a customer is put into
     void setCheckoutLine(int registerIndex) {
         this.registerIndex = registerIndex;
     }
    
+    //returns which checkout line the customer is in
     int getCheckoutLine(){
         return this.registerIndex;
     }
    
+    //sets the time the customer started waiting
     public void setStartWaitTime(double i){
         this.startWaitTime = i;
     }
    
+    //sets the time the customer spent waiting
     public void setWaitTime(double i){
         this.waitTime = i;
     }
    
+    //sets the register type that the customer is using (Standard or Self)
     public void setRegType(String r){
         this.registerType = r;
     }
    
+    //returns the time the customer started waiting
     public double getStartWaitTime(){
         return this.startWaitTime;
     }
    
+    //returns the time the customer spent waiting
     public double getWaitTime(){
         return this.waitTime;
     }
    
+    //returns the type of register the customer is using (Standard or Self)
     public String getRegType(){
         return this.registerType;
     }
