a183b656149f83d7cf4d74be358f1f03431b34ab
Travis Wahl <twahl1991@gmail.com>
2021-02-13 21:18:24 -0500

    Create NewClass.java

---
+++ src/NewClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author twahl
+ */
+public class NewClass {
+    int x = 20;
+    int n = 5;
+    int sum = 0;
+   
+    
+    int sum(){
+    for (int i = 0; i < n; i++) {
+        int x = 30;
+        sum = sum + x;
+    }
+        System.out.println(sum);
+    return sum;
+    }
+}
